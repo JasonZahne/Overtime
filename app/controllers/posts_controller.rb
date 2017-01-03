4e736d6c6a5e57835e2b25dd3abf6364c2898f6a
Jason <jasonzahne@gmail.com>
2017-01-03 12:52:36 -0700

    finished month 2 week 2 thursday vids

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	
 	def index
-		@posts = Post.all
+		@posts = Post.posts_by current_user
 	end
 
 	def new
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 		authorize @post
 	end
 
-	def update 
+	def update
 		authorize @post
 		
 		if @post.update(post_params)
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
 	private
 
 	  def post_params
-	  	params.require(:post).permit(:date, :rationale, :status)
+	  	params.require(:post).permit(:date, :rationale, :status, :overtime_request)
 	  end
 
 	  def set_post
